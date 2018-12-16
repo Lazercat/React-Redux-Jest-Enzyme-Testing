3213ef2a4559638bca0f6a7f30ad3ffe89d83e9a
Jesse Lewis <jesseaustinlewis@gmail.com>
2018-12-16 18:43:39 -0500

    code cleanup for functional components.

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
-export class Stack extends Component{
-	render(){
-		const { title, cards } = this.props.stack;
-
-		return(
-			<React.Fragment>
-				<Link className="link-home" to="/">
-					<h4>Home</h4>
-				</Link>
-				<h3>{title}</h3>
-				<br />
-				{cards.map( card => {
-					return (
-						<Card 
-							key={card.id}
-							card={card}
-						>
-							{card.prompt}
-						</Card>
-					);
-				})}
-			</React.Fragment>
-		);
-	}
-}
+export const Stack = ({ stack: { title, cards }}) => {
+	return(
+		<React.Fragment>
+			<Link className="link-home" to="/">
+				<h4>Home</h4>
+			</Link>
+			<h3>{title}</h3>
+			<br />
+			{cards.map( card => {
+				return (
+					<Card 
+						key={card.id}
+						card={card}
+					>
+						{card.prompt}
+					</Card>
+				);
+			})}
+		</React.Fragment>
+	);
+};
 
 function mapStateToProps(state){ 
 	return { stack: state.stack };
